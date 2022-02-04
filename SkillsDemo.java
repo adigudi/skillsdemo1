55c7da84aefaa6df4d35d2164e748a5639121618
Adi Gudi <agudi24@gmail.com>
2022-02-03 21:50:58 -0800

    added failing test

--- SkillsDemo.java
+++ SkillsDemo.java
@@ -1,1 +1,5 @@
-public class SkillsDemo {}
+public class SkillsDemo {
+    public static int addition(int a, int b){
+        return a + b;
+    }
+}
---
+++ SkillsDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillsDemoTest{
+    @Test
+    public void testAddition(){
+        assertEquals(3 , SkillsDemo.addition(2, 2));
+    }
+}
